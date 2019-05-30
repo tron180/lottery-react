19b4233b3ac21d04693f4b2d3abdbf9f0aeed2d3
Yash Mishra <yash.mishra@raweng.com>
2019-05-30 18:31:13 +0530

    Updates

---
+++ precache-manifest.a5a6448f8c01e5d21ee04a2d302e0034.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec7d39ac23caf4ede08a0dcbd664b92c",
+    "url": "/lottery-react/index.html"
+  },
+  {
+    "revision": "e8986ced769cb2a1de03",
+    "url": "/lottery-react/static/css/main.e0ce4c25.chunk.css"
+  },
+  {
+    "revision": "e3e1726d43da044c5a6a",
+    "url": "/lottery-react/static/js/2.7cddf10a.chunk.js"
+  },
+  {
+    "revision": "e8986ced769cb2a1de03",
+    "url": "/lottery-react/static/js/main.6446420f.chunk.js"
+  },
+  {
+    "revision": "86c68faabb279f23387f",
+    "url": "/lottery-react/static/js/runtime~main.e04e6bb0.js"
+  }
+]);
